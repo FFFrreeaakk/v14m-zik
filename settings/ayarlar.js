1576a75f982e3607940b63dcfa0fdda9fd42b682
FFFrreeaakk <106516925+FFFrreeaakk@users.noreply.github.com>
2022-10-27 07:35:07 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  TOKEN: process.env.TOKEN || "MTAzNDg5NTQ5NjI1MjI0ODE0NQ.G1LPjA.u12M85VGwfBmFQNiVk3KPjEPXLDt6zaS0TXHmI",  // bot token gir :)
+  PREFIX: process.env.PREFIX || ".", // bot prefix
+  SAHİP: process.env.SAHİP || "911992271858188388", //sahip id :()
+}
